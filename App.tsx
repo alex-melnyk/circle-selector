d27284940607ae2b7c2c3175db62c18f6f038c24
Alex Melnyk <ask.melnyk@gmail.com>
2020-01-15 18:04:26 +0200

    Rotation implemented.

--- App.tsx
+++ App.tsx
@@ -5,9 +5,16 @@ import { Circle } from './src/components';
 export default function App() {
   return (
     <View style={styles.container}>
-
-      <Circle/>
-
+      <Circle
+        icons={[
+          { name: 'pencil' },
+          { name: 'globe' },
+          { name: 'home' },
+          { name: 'rocket' },
+          { name: 'car' },
+          { name: 'bluetooth' },
+        ]}
+      />
     </View>
   );
 }
--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,40 +1,88 @@
-import React, { useState } from 'react';
-import { Animated, View } from 'react-native';
+import React, { useCallback, useMemo, useState } from 'react';
+import { Animated, Text, View } from 'react-native';
 import {
   PanGestureHandler,
   PanGestureHandlerGestureEvent,
   PanGestureHandlerStateChangeEvent,
   State
 } from 'react-native-gesture-handler';
+import { FontAwesome } from '@expo/vector-icons';
 
-type Props = {
-  size: number;
+type Icon = {
+  name: string;
 };
 
-export const Circle: React.FC<Props> = ({ size = 250 }) => {
+type Props = {
+  size?: number;
+  iconSize?: number;
+  icons: Icon[];
+};
+
+export const Circle: React.FC<Props> = ({
+  size = 150,
+  iconSize = 20,
+  icons
+}) => {
   const [rotation, setRotation] = useState(0);
-  const angleValue = new Animated.Value(0);
+  const [angleValue, setAngleValue] = useState(0);
+  const animatedAngle = useMemo(() => new Animated.Value(0), []);
 
-  const handleGestureEvent = (event: PanGestureHandlerGestureEvent) => {
-    angleValue.setValue(rotation + event.nativeEvent.translationX);
-    // console.log(event.nativeEvent.translationX);
-  };
+  const radius = useMemo(() => size / 2, [size]);
+  const iconOffset = useMemo(() => radius - iconSize / 2, [radius, iconSize]);
+  const iconsDegree = useMemo(() => 360 / icons.length, [icons]);
 
-  const handleHandlerStateChange = (event: PanGestureHandlerStateChangeEvent) => {
+  const handleGestureEvent = useCallback((event: PanGestureHandlerGestureEvent) => {
+    const angle = rotation + event.nativeEvent.translationX;
+
+    setAngleValue(angle);
+    animatedAngle.setValue(angle);
+  }, [angleValue, rotation]);
+
+  const handleHandlerStateChange = useCallback((event: PanGestureHandlerStateChangeEvent) => {
     switch (event.nativeEvent.state) {
       case State.END:
-        const endingValue = rotation + event.nativeEvent.translationX;
+        const angle = rotation + event.nativeEvent.translationX;
 
-        setRotation(endingValue);
-        angleValue.setValue(endingValue);
+        // const diff = endingValue % iconsDegree;
+        // const setting = endingValue + (iconsDegree - diff);
+
+        const comb = angle > 360 ? angle - 360 : angle < 0 ? 360 + angle : angle;
+
+        setRotation(comb);
+        setAngleValue(comb);
+        animatedAngle.setValue(comb);
+
         break;
     }
-  };
+  }, [angleValue, rotation]);
 
-  const angle = angleValue.interpolate({
-    inputRange: [0, 720],
-    outputRange: ['0deg', '720deg']
-  });
+  const iconsList = useMemo(() => icons.map((icon, idx) => {
+    const angle = idx * iconsDegree - 90 + angleValue;
+    const x = iconOffset * Math.cos(Math.PI * 2 * angle / 360) + iconOffset;
+    const y = iconOffset * Math.sin(Math.PI * 2 * angle / 360) + iconOffset;
+
+    return (
+      <View
+        key={`icon_${idx}`}
+        style={{
+          position: 'absolute',
+          left: x - 2,
+          top: y - 2,
+          width: iconSize + 4,
+          height: iconSize + 4,
+          justifyContent: 'center',
+          alignItems: 'center'
+          // backgroundColor: 'yellow'
+        }}
+      >
+        <FontAwesome
+          name={icon.name}
+          size={iconSize}
+          color="black"
+        />
+      </View>
+    );
+  }), [icons, iconOffset, angleValue]);
 
   return (
     <>
@@ -45,31 +93,20 @@ export const Circle: React.FC<Props> = ({ size = 250 }) => {
         <View>
           <Animated.View
             style={{
-              alignItems: 'center',
-              justifyContent: 'center',
               width: size,
               height: size,
               borderRadius: size / 2,
-              backgroundColor: '#9BFF00',
-              transform: [
-                { rotateZ: angle }
-              ]
+              // overflow: 'hidden'
             }}
           >
             <View
               style={{
-                width: 100,
-                height: 100,
-                backgroundColor: '#FF9B00'
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
               }}
             >
-              <View
-                style={{
-                  width: 25,
-                  height: 25,
-                  backgroundColor: '#FF009B'
-                }}
-              />
+              {iconsList}
             </View>
           </Animated.View>
         </View>
